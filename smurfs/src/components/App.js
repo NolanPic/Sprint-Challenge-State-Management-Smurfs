64a91cace3058890043e46d3939eb6af00f333fb
Nolan Picini <nolanpic@gmail.com>
2020-01-24 10:18:55 -0800

    Hey this is context

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -18,7 +18,7 @@ const App = () => {
   return (
     <div className="App">
       <SmurfsContext.Provider value={{ state, dispatch }}>
-        <h1>SMURFS! 2.0 W/ Redux</h1>
+        <h1>SMURFS! 2.0 W/ Context</h1>
         <AddSmurf />
         <SmurfList />
       </SmurfsContext.Provider>
