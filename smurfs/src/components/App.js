f11adce202f90131b8403f89a469545918b7813e
Nolan Picini <nolanpic@gmail.com>
2020-01-24 08:40:37 -0800

    Add reducer and refactor App into functional

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,23 +1,23 @@
-import React, { Component } from "react";
+import React, { useReducer } from "react";
+import reducer from '../reducers';
 import "./App.css";
 import { SmurfsContext } from '../contexts/SmurfsContext';
-class App extends Component {
 
-  state = {
+const App = () => {
+
+  const [state] = useReducer(reducer, {
     smurfs: [],
     isLoading: false,
     error: null
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <SmurfsContext.Provider value={this.state}>
-          <h1>SMURFS! 2.0 W/ Redux</h1>
-        </SmurfsContext.Provider>
-      </div>
-    );
-  }
-}
+  });
+  
+  return (
+    <div className="App">
+      <SmurfsContext.Provider value={state}>
+        <h1>SMURFS! 2.0 W/ Redux</h1>
+      </SmurfsContext.Provider>
+    </div>
+  );
+};
 
 export default App;
